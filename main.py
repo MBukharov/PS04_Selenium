e26bffc7690f5b4dab4e9759e07c58a403de588e
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2024-10-18 16:57:05 +0300

    beginning

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 browser = webdriver.Chrome()
 browser.get("https://ru.wikipedia.org/wiki/")
-time.sleep(5)
+# time.sleep(5)
 
+zapros = input("введите запрос: ")
+
+search_line = browser.find_element(By.ID,"searchInput")
+search_line.send_keys(zapros)
+search_line.send_keys(Keys.RETURN)
+time.sleep(5)
