b65f7a3aae752f8cc03394b0fd8e4a4c79c7d44f
MBukharov <mikhail.bukharov@gmail.com>
2024-10-21 11:51:31 +0300

    paragraphs are ready

--- main.py
+++ main.py
@@ -8,8 +8,24 @@ browser.get("https://ru.wikipedia.org/wiki/")
 # time.sleep(5)
 
 zapros = input("введите запрос: ")
-
 search_line = browser.find_element(By.ID,"searchInput")
 search_line.send_keys(zapros)
-search_line.send_keys(Keys.RETURN)
-time.sleep(5)
+browser.find_element(By.ID,"searchButton").click()
+while True:
+
+    # search_line.send_keys(Keys.RETURN)
+    selection = input("Выберите действие: \n1 - листать параграфы страницы\n2 - перейти на связанную страницу\nq - выйти\n")
+    if selection == "1":
+        paragraphs = browser.find_elements(By.TAG_NAME,"p")
+        print("\nДля открытия следующего параграфа нажмите Enter. Для выхода введите q")
+        for paragraph in paragraphs:
+            print(paragraph.text)
+            p = input()
+            if p == 'q':
+                break
+    elif selection == "2":
+        pass
+    elif selection == "q":
+        break
+
+# time.sleep(5)
